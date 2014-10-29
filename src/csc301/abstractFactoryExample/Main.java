e84d2e51f62f583eb29b6a779901b0a55a67dc41
Joey Freund <joey@cs.toronto.edu>
2014-10-29 09:33:55 -0400

    Moving the app code to its own package, so that the imports will indicate the dependencies.

--- src/csc301/abstractFactoryExample/Main.java
+++ src/csc301/abstractFactoryExample/Main.java
@@ -1,12 +1,17 @@
 package csc301.abstractFactoryExample;
 
+import csc301.abstractFactoryExample.tripAdvisor.TripAdvisor;
+import csc301.abstractFactoryExample.tripAdvisor.TripAdvisor1;
+import csc301.abstractFactoryExample.tripAdvisor.TripAdvisor2;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		// We can decide which line to use ...
-		TripAdvisor advisor = new TripAdvisor1();
-//		TripAdvisor advisor = new TripAdvisor2();
+		 
+		TripAdvisor advisor;
+		// Now we can use whichever implementation we choose 
+		advisor = new TripAdvisor1();
+		advisor = new TripAdvisor2();
 		
 		// Regardless of which trip advisor we instantiated, we can run our test code ...
 		System.out.println("Now I can test the trip advisor ...");
