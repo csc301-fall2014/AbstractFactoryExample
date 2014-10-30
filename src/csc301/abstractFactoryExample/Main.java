8dfe087a2a757194b4fdc8f273b0418f77aa14db
Joey Freund <joey@cs.toronto.edu>
2014-10-30 12:24:53 -0400

    Adding some comments

--- src/csc301/abstractFactoryExample/Main.java
+++ src/csc301/abstractFactoryExample/Main.java
@@ -1,8 +1,9 @@
 package csc301.abstractFactoryExample;
 
+import java.lang.reflect.InvocationTargetException;
+
 import csc301.abstractFactoryExample.tripAdvisor.TripAdvisor;
 import csc301.abstractFactoryExample.tripAdvisor.TripAdvisorFactory;
-import csc301.abstractFactoryExample.tripAdvisor.TripAdvisorFactoryImpl1;
 
 public class Main {
 
@@ -19,8 +20,28 @@ public class Main {
 	
 
 	
-	public static void main(String[] args) {
-		run(new TripAdvisorFactoryImpl1());
+	/**
+	 * A helper function that creates an instance of class, given the class's name.
+	 * NOTE: 
+	 * This is (1) a fairly advanced topic (2) could be done better.
+	 * It is not part of the course material, I just wanted you to see that it is possible
+	 * to write a program that depends solely on the domain's interfaces, without 
+	 * mentioning any specific implementation. 
+	 */
+	private static TripAdvisorFactory createFactory(String className) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException{
+		Class<?> clazz = Class.forName(className);
+		return (TripAdvisorFactory) clazz.getConstructor().newInstance();
+	}
+	
+	
+	
+	public static void main(String[] args){
+		try{
+			run(createFactory(System.getenv("FACTORY_IMPL")));
+		} catch (Exception e){
+			System.out.println("Ooops ... " + e.getMessage());
+		}
 	}
 
+	
 }
