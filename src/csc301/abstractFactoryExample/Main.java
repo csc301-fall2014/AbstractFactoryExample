9bc83bba54bdb12c2d76b985f960946ba13c46e5
Joey Freund <joey@cs.toronto.edu>
2014-11-04 09:26:39 -0500

    Separating the Main class into Main and Application, in order to make the decoupling a little clearer.

--- src/csc301/abstractFactoryExample/Main.java
+++ src/csc301/abstractFactoryExample/Main.java
@@ -1,26 +1,11 @@
 package csc301.abstractFactoryExample;
 
-import csc301.abstractFactoryExample.tripAdvisor.TripAdvisor;
-import csc301.abstractFactoryExample.tripAdvisor.TripAdvisorFactory;
 import csc301.abstractFactoryExample.tripAdvisor.TripAdvisorFactoryImpl1;
 
 public class Main {
 
-	
-	// Notice how the code in this function only depends on the two interfaces:
-	// TripAdvisorFactory and TripAdvisor.
-	public static void run(TripAdvisorFactory factory){
-		TripAdvisor advisor = factory.getInstance();
-		
-		System.out.println("Now I can test the trip advisor ...");
-		double price = advisor.getCheapestPrice("Toronto", "Montreal");
-		System.out.println("A cheapest trip from Toronto to Montreal costs " + price + "$.");
-	}
-	
-
-	
 	public static void main(String[] args) {
-		run(new TripAdvisorFactoryImpl1());
+		new Application(new TripAdvisorFactoryImpl1()).run();
 	}
 
 }
